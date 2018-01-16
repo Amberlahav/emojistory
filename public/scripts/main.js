2ac18acefdc4b55e357e8a773b065a4d29099917
Amber Lahav <amberlahav@gmail.com>
2018-01-15 20:15:50 -0500

    adding files for this project

---
+++ public/scripts/main.js
@@ -0,0 +1,16 @@
+// listening for the key down event, and when that happens,run function playSound
+// the (e) is the event describing the object/what happens which has info like what key was hit, key code,etc.
+
+
+// document.addEventListener('keydown', showEmoji);
+
+// function showEmoji(e) {
+//     // selecting every audio file with the source attribute that has a keyCode in its data, and storing in a variable
+
+//     // const emoji = document.querySelector(`.emoji[data-key="${e.keyCode}"]`);
+
+//     // emoji.classList.toggle('showEmoji');
+
+//     console.log(e);
+// }
+"use strict";
